98759c78e4f1c2d057f869b05da7fc1cc10deaed
Amy Baer <amyjbaer@umich.edu>
2016-06-21 14:51:38 -0400

    try catch

--- index.php
+++ index.php
@@ -8,13 +8,15 @@
 <h1>Website</h1>
 <button>Push me!</button>
 <?php 
-echo '1';
-exit;
 $servername = "104.236.198.230";
 $username = "Amy";
 $passwd = "password";
+try {
 $conn = new PDO("mysql:host=104.236.198.230:3306;dbname=user", $username, $passwd);
-echo $conn;
+}
+catch(\Exception $e) {
+	echo $e->getMessage();
+}
 $sql = "SELECT * FROM user.user";
 $result = $conn->query($sql);
 var_dump($reuslt->fetchAll()); 
