77e90eb1f4d0a5586fbe696fed8f2a0222dd1886
Amy Baer <amyjbaer@umich.edu>
2016-06-21 15:15:10 -0400

    added css and jquery

--- index.php
+++ index.php
@@ -1,12 +1,26 @@
 <!DOCTYPE html>
+<style>
+p {
+	display: none;
+}
+</style>
 <html>
 <head>
 <meta charset="ISO-8859-1">
-<title>Insert title here</title>
+<title>My Website</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+	$('button').click(function() {
+		$('p').css('display', 'block');
+	});
+	
+});
+</script>
 </head>
 <body>
 <h1>Website</h1>
-<button>Push me!</button>
+<button>Push me to see everyone's names!</button>
 <?php 
 $servername = "104.236.198.230";
 $username = "amy";
